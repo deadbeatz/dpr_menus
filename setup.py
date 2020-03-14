612c92bac4cf00baecb1b514041aa22c617d2899
Analog <analog@deadbeatz.org>
2020-03-14 14:48:01 -0600

    Renamed package for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'dpr-menus',         # How you named your package folder (MyLib)
-  packages = ['dpr-menus'],   # Chose the same as "name"
+  name = 'dprmenus',         # How you named your package folder (MyLib)
+  packages = ['dprmenus'],   # Chose the same as "name"
   version = '1.9.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'dPR Team: Menus for Mystic BBS',   # Give a short description about your library
@@ -13,8 +13,6 @@ setup(
   install_requires=[],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: BBS Utils',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 2',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 2.7',
