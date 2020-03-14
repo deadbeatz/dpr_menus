3ad2c0d8adfbfd209f6ed095ce604bb5e69d8dd7
Analog <analog@deadbeatz.org>
2020-03-14 09:37:13 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'dpr_menus',
-    packages = ['dpr_menus'],
-    version = '1.9',  # Ideally should be same as your GitHub release tag varsion
-    description = 'dPR Team: BBS Menuing System for Mystic BBS',
-    author = 'Analog',
-    author_email = 'analog@deadbeatz.org',
-    url = 'https://github.com/deadbeatz/dpr_menus',
-    download_url = 'https://github.com/deadbeatz/dpr_menus/archive/v1.9.tar.gz',
-    keywords = ['dpr', 'dpr_menus'],
-    classifiers = [],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dpr-menus-Analog", # Replace with your own username
+    version="1.9.0",
+    author="Analog",
+    author_email="analog@deadbeatz.org",
+    description="dPR menuing system for Mystic BBS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deadbeatz/dpr_menus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+		python_requires='>=2.7',
 )
